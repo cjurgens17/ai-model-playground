5fda2ad19c8f8847847aeb6478126cb1c85f5095
Chris Jurgens <chrisjurgens12317@gmail.com>
2026-07-19 14:22:43 -0400

    Merge all chapters into a single txt file so training data is condensed

---
+++ parse-pdf/merged-chapters/merge-all-chapters.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+
+OUTPUT_FILE = Path("merged-chapters.txt")
+INPUT_DIR = Path("../ddia-chapters")
+
+def merge_all_chapters():
+    with OUTPUT_FILE.open("w", encoding="utf-8") as outfile:
+        for txt_file in sorted(INPUT_DIR.glob("*.txt")):
+            outfile.write(txt_file.read_text(encoding="utf-8"))
+            outfile.write("\n")
+
+merge_all_chapters()
